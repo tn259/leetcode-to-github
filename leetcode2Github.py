9852c57ee0a14f4126d91f2bd0ba5353341ee8e3
Tim <tjnewman2013@gmail.com>
2020-02-17 22:19:10 +0000

    Add scrape method for accepted submissions - not yet MVP

--- leetcode2Github.py
+++ leetcode2Github.py
@@ -36,10 +36,38 @@ class leetcode:
     print("Login succeeded")
     self.loggedIn = True
 
+  def scrape_latest_accepted_submissions(self):
+    if self.loggedIn:
+      self.driver.get("https://leetcode.com/progress")
+
+      print("Getting progress page")
+
+      # wait for response
+      try:
+        WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "submission-status"))
+        )
+      except selenium.common.exceptions.NoSuchElementException:
+        print("Progress page failed to load")
+        return
+    
+      print("Progress page loaded")
+
+      accepted_submissions_table = self.driver.find_element_by_xpath("//table[@id='recent_ac_list']")
+
+      pdb.set_trace()
+      for row in accepted_submissions_table.find_elements_by_xpath(".//tr"):
+        pass
+        #time_str = row.find_element_by_xpath(".//td[1]").text
+        #href = row.find_element_by_xpath(".//td[2]/a/@href")
+        #print(time_str)
+        #print(href)
+
 
 def main():
   lc = leetcode()
   lc.login()
+  lc.scrape_latest_accepted_submissions()
 
 if __name__ == "__main__":
   main()
